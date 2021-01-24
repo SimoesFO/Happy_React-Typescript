21ac0e934939ec40158d176c2c6c9f9283d71d5c
unknown <ti7@orthodonticbrasil.com.br>
2021-01-24 10:24:14 -0300

    Ending App Moblile.

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: 'https://localhost:3333',
-    baseURL: 'http://192.168.15.4:3333',
+    baseURL: 'https://localhost:3333',
+    //baseURL: 'http://192.168.15.4:3333',
 });
 
 export default api;
--- server/src/views/images_view.ts
+++ server/src/views/images_view.ts
@@ -4,8 +4,8 @@ export default {
     render(image: Image) {
         return {
             id: image.id,
-            //url: `http://localhost:3333/uploads/${image.path}`
-            url: `http://192.168.15.4:3333/uploads/${image.path}`
+            url: `http://localhost:3333/uploads/${image.path}`
+            //url: `http://192.168.15.4:3333/uploads/${image.path}`
         }
     },
 
